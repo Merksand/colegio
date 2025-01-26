d980dfe5fb1a1e42fc226dddaec0a7cd0eb123bd
Merksand <miguelangelsp7161@gmail.com>
2025-01-25 21:34:53 -0400

    s

--- src/app/api/universidades/[id]/route.js
+++ src/app/api/universidades/[id]/route.js
@@ -5,31 +5,15 @@ export async function PUT(request, { params }) {
     try {
         const { id } = params;
         const body = await request.json();
-
-        // Destructurando los datos enviados desde el frontend
         const { Nombre_Uni, Tipo_Uni, Sede_Uni } = body;
 
-        // Validar que los campos obligatorios estén presentes
-        if (!Nombre_Uni ) {
-            return NextResponse.json(
-                { error: "Los campos Nombre y Estado son obligatorios." },
-                { status: 400 }
-            );
-        }
-
-        // Reemplazar valores undefined o vacíos por null
         const result = await query(
             `UPDATE TbUniversidad SET 
                 Nombre_Uni = ?, 
                 Tipo_Uni = ?, 
                 Sede_Uni = ?
             WHERE Id_Universidad = ?`,
-            [
-                Nombre_Uni,
-                Tipo_Uni || null,
-                Sede_Uni || null,
-                id,
-            ]
+            [Nombre_Uni, Tipo_Uni, Sede_Uni, id]
         );
 
         if (result.affectedRows === 0) {
@@ -41,20 +25,19 @@ export async function PUT(request, { params }) {
 
         return NextResponse.json({ message: "Universidad actualizada exitosamente" });
     } catch (error) {
-        console.error("Error al actualizar universidad:", error);
+        console.error("Error al actualizar:", error);
         return NextResponse.json(
-            { error: "Error interno del servidor", details: error.message },
+            { error: "Error al actualizar universidad", details: error.message },
             { status: 500 }
         );
     }
 }
 
-
 export async function DELETE(request, { params }) {
     try {
         const { id } = params;
         const result = await query(
-            'UPDATE TbUniversidad SET Estado_Uni = "BA" WHERE Id_Universidad = ?',
+            'DELETE FROM TbUniversidad WHERE Id_Universidad = ?',
             [id]
         );
 
